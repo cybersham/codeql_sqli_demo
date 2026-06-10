a6f0d29f96642913d6c648cdc068673507329cd4
cybersham <learningcybersham@gmail.com>
2026-06-10 15:54:37 +0530

    Fixing typo errors

--- app.py
+++ app.py
@@ -1,19 +1,16 @@
-import sqllite3
+import sqlite3
 
 def get_username(username):
-    connection = sqllite3.connect("users.db")
+    connection = sqlite3.connect("users.db")
     cursor = connection.cursor()
     query = "SELECT * FROM users WHERE username = '" + username + "'"
     cursor.execute(query)
-
     return cursor.fetchall()
 
 def main():
-
     user_input = "admin' OR '1'='1"
-    results = get_user(user_input)
+    results = get_username(user_input)
     print(results)
 
-if __name__ == "__main__""
+if __name__ == "__main__":
     main()
-
