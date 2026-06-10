4cd9c7793f31a801ded9ea96e79f24e4a48a34aa
cybersham <learningcybersham@gmail.com>
2026-06-10 16:16:05 +0530

    to figure out the missing source or sink

--- app.py
+++ app.py
@@ -1,22 +1,11 @@
 import sqlite3
 import sys
-from urllib.parse import parse_qs 
 
-def get_username(username):
-    connection = sqlite3.connect("users.db")
-    cursor = connection.cursor()
-    query = "SELECT * FROM users WHERE username = '" + username + "'"
-    cursor.execute(query)
-    return cursor.fetchall()
+conn = sqlite3.connect("users.db")
+cursor = conn.cursor()
 
-def main():
-    if len(sys.argv) > 1:
-        fake_url_input = f"user={sys.argv[1]}"
-        parsed = parse_qs(fake_url_input)
-        user_input = parsed['user'][0] 
-        
-        results = get_username(user_input)
-        print(results)
+user_input = sys.argv[1]
 
-if __name__ == "__main__":
-    main()
+query = "SELECT * FROM users WHERE username = '" + user_input + "'"
+
+cursor.execute(query)
